21a536a7e6c257352b398e6ed8bd143b1937ebe7
Nhi Tran <nhitran@Nhis-MBP.localdomain>
2022-06-28 17:19:26 +1000

    added image to listings page

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -80,6 +80,6 @@ class ListingsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def listing_params
-      params.require(:listing).permit(:name, :description, :condition, :size, :price, :sold, :user_id, :category_id)
+      params.require(:listing).permit(:name, :description, :condition, :size, :price, :sold, :user_id, :category_id, :picture)
     end
 end
