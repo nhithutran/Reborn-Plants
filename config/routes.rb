17f4a5fafce44de7550b9cb6ecf328d05158c7f9
Nhi Tran <nhitran@Nhis-MacBook-Pro.local>
2022-07-06 15:33:53 +1000

    add authentication to orders controller change success route

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,6 @@ Rails.application.routes.draw do
 
   post "listings/:id/order", to: "listings#place_order", as: "place_order"
 
-  get 'page/success', to: 'page#success', as: "order_success"
+  get 'orders/success', to: 'orders#success'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
